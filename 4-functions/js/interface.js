7efee32f63aba6832198f62580ac64e3a3b00671
juliakuchman <juliakuchman@gmail.com>
2015-10-21 19:04:14 -0700

    hw temp

--- 4-functions/js/interface.js
+++ 4-functions/js/interface.js
@@ -1,3 +1,6 @@
+
+// Pythagorean Theorem 
+
 var pyA = document.querySelector('.py-a');
 var pyB = document.querySelector('.py-b');
 var pyC = document.querySelector('.py-c');
@@ -13,11 +16,6 @@ function calcPy(event) {
   }
 }
 
-
-
-
-
-
 var r = document.querySelector('.circum-r');
 var c = document.querySelector('.circum-c');
 
@@ -31,21 +29,25 @@ function calcC(event) {
   }
 }
 
+// Fahrenheit to Celcius and vice versa
 
-
-
-
-
-
+//variables are defined by string objects that user puts in, names are what referenced in html
 var tempF = document.querySelector('.temp-f');
 var tempC = document.querySelector('.temp-c');
 
+//what is this? MDN says keyup event is fired when a key is released
+//think this calls functions calcFtoC when key released
 tempF.addEventListener('keyup', calcFtoC);
 tempC.addEventListener('keyup', calcCtoF);
 
+//CalcFtoC is function
+//assuming event is capturing what text is typed in?
+//event is the parameter/argument
+//tempC is return 
+//can we take out the calcFahrenheitToCelcius and replace with own?
 function calcFtoC(event) {
-  if (tempF.value.length > 0) {
-    tempC.value = calcFahrenheitToCelcius(tempF.value)
+  if (tempF.value.length > 0) 
+    tempC.value = (5/9)* tempF.value-32
   } else {
     tempC.value = '';
   }
@@ -53,7 +55,7 @@ function calcFtoC(event) {
 
 function calcCtoF(event) {
   if (tempC.value.length > 0) {
-    tempF.value = calcCelciusToFahrenheit(tempC.value)
+    tempF.value = tempC.value * (9/5) + 32
   } else {
     tempF.value = '';
   }
