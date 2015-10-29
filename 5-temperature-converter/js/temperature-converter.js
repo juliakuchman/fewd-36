90c448df74479e8151851e99f2bc2ed67f49a940
juliakuchman <juliakuchman@gmail.com>
2015-10-28 17:54:31 -0700

    10.28

---
+++ 5-temperature-converter/js/temperature-converter.js
@@ -0,0 +1,12 @@
+    
+	addEventListener("click", function(IDK))
+
+    function calcCelciusToFahrenheit(c){
+      var f = c * (9/5) + 32;
+      return f;
+      }
+
+    function calcFahrenheitToCelcius(f){
+      var c = (5/9) * (f-32) 
+      return c; 
+      } 
